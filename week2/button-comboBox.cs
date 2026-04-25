a30dc2bfddabb33812c3d3cb290d7223f1003e52
Ayşe Dilara Koçar <dilarakocar@gmail.com>
2026-04-25 22:56:17 +0300

    Create button-comboBox.cs

---
+++ week2/button-comboBox.cs
@@ -0,0 +1,14 @@
+namespace WinFormsApp6
+{
+public partial class Form1 : Form
+{
+public Form1()
+{
+InitializeComponent();
+}
+private void button1_Click(object sender, EventArgs e)
+{
+comboBox1.Items.Add("Edirne");
+}
+}
+}
