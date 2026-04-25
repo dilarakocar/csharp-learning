3baf99205961a17913e93066f3ac6d58d996a870
Ayşe Dilara Koçar <dilarakocar@gmail.com>
2026-04-25 20:34:52 +0300

    Add Python variables example

---
+++ variables.py
@@ -0,0 +1,12 @@
+using System;
+using static System.Collections.Specialized.BitVector32;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a,b;
+        a=25; b=62;
+        Console.WriteLine("A={0}",a);
+        Console.WriteLine("B={0}",b);
+    }
+}
