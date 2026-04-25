6e46d75758a5aa6f823f11bb82ddbfeae60da9b3
Ayşe Dilara Koçar <dilarakocar@gmail.com>
2026-04-25 20:52:40 +0300

    Add C# console colors example

---
+++ console-colors.cs
@@ -0,0 +1,13 @@
+
+using System;
+using static System.Collections.Specialized.BitVector32;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.Clear();
+        Console.WriteLine("OKAN UNIVERSITY");
+    }
+}
